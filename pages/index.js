8eb73adb1cd58725eb7319b2d989ca77cb64850c
Johnny Lira <johnnyjlira@gmail.com>
2024-10-02 11:42:00 +0000

    Mensagem pra Jessica

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 export default function index(){
-    return(
-        <h1>Minha Página</h1>
+    return(<>
+        <h1>Jéssica, te amo demais!!! </h1>
+        <h2>Sou grato por te ter na minha vida ❤️</h2>
+        </>
     )
 }
