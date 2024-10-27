7e5068b07890967e8f15f6c5f158477ec32947ba
Johnny-Lira <johnnyjlira@gmail.com>
2024-10-27 18:31:37 -0300

    adiciona scripts lint:check e lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Clone do TabNews 
+
+Clone do TabNews
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export default function index(){
-    return(<>
-        <h1>Jéssica, te amo demais!!! </h1>
-        <h2>Sou grato por te ter na minha vida ❤️</h2>
-        </>
-    )
+function Home() {
+  return (
+    <>
+      <h1>Jéssica, te amo demais!!! </h1>
+      <h2>Sou grato por te ter na minha vida ❤️</h2>
+    </>
+  );
 }
+
+function teste() {
+  console.log("Teste");
+}
+
+export default Home();
